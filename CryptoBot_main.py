fc6237f642bf1c57718541e5ae10a50f8d184974
Frantisek Grossmann <frantisek.grossmann33@gmail.com>
2017-12-07 14:06:42 +0100

    Add files via upload

---
+++ CryptoBot_main.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import time
+import sys
+
+def main(argv):
+    period = 10 
+    
+    while True:
+        print "period"
+        time.sleep(period) #Suspend execution of the current thread for the given number of seconds.
+        
+if Cryptobot
+
