0fd55328e061a538d8008667fe41a6a7ff7d2913
Frantisek Grossmann <frantisek.grossmann33@gmail.com>
2018-03-20 12:21:47 +0100

    Update CryptoBot_main.py

--- CryptoBot_main.py
+++ CryptoBot_main.py
@@ -6,16 +6,5 @@
 import time
 import sys
 import datetime
-
-from poloniex import Poloniex
-
-polo = poloniex.Poloniex('Public key','Secret key')
-balance = polo('returnBalances')
-print("I have %s BTC!" % balance['BTC'])
-
-#def main(argv):
-#    period = 10 
-    
-
-        
+   
 
