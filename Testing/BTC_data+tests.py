2134a51868577838c9d29db7e5d17eec88db109e
Frantisek Grossmann <frantisek.grossmann33@gmail.com>
2018-03-21 14:31:38 +0100

    Update BTC_data+tests.py

--- Testing/BTC_data+tests.py
+++ Testing/BTC_data+tests.py
@@ -2,22 +2,12 @@
 # coding: utf-8
 
 # In[9]:
-
+import numpy as np
 import pandas as pd
-get_ipython().magic('matplotlib inline')
+%matplotlib inline
 import matplotlib.pyplot as plt
 #locating the dataset
 #boolean or list of ints or names or list of lists or dict, default False
-btc_price=pd.read_csv("Desktop/coinbaseUSD_1-min_data_2014-12-01_to_2018-01-08.csv",parse_dates=True,index_col=0)
-btc_price["Close"].plot()
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
+btc_price=pd.read_csv("Desktop/BTC-USD.csv",parse_dates=True,index_col=0)
+btc_price["Close"]["2016-12-31":"2017-12-31"].plot()
+pd.rolling_mean(btc_price["Close"]["2016-12-31":"2017-12-31"],5).plot()
